db85a91e7c8b29fcf4ac698e65c5a2081b6c3dad
Richard joy <richardjoy530@gmail.com>
2022-04-02 12:53:38 +0530

    Basic stored procedures for PhotographerVoteDetails

---
+++ Database/Insert_PhotographerVoteDetails.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[Insert_PhotographerVoteDetails]
+	@Id                     int,
+    @ThemeId                int,
+    @PhotographerId         int,
+    @FirstId                int,
+    @SecondId               int,
+    @ThirdId                int
+AS
+
+INSERT INTO [dbo].[PhotographerVoteDetails]
+           ([Id]
+           ,[ThemeId]
+           ,[PhotographerId]
+           ,[FirstId]
+           ,[SecondId]
+           ,[ThirdId])
+     OUTPUT [INSERTED].[Id]
+     VALUES
+           (@Id
+           ,@ThemeId
+           ,@PhotographerId
+           ,@FirstId
+           ,@SecondId
+           ,@ThirdId)
+
+RETURN 0
