e3f5668fae853fe6c9b9014af363163b421875ce
Richard joy <richardjoy530@gmail.com>
2022-04-02 12:53:38 +0530

    Basic stored procedures for PhotographerVoteDetails

---
+++ Database/Delete_PhotographerVoteDetails.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Delete_PhotographerVoteDetails]
+	@Id int
+AS
+
+DELETE FROM [dbo].[PhotographerVoteDetails]
+      WHERE [Id] = @Id
+
+RETURN 0
---
+++ Database/Update_PhotoprapherVoteDetails.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[Update_PhotoprapherVoteDetails]
+	@Id                     int,
+    @ThemeId                int,
+    @PhotographerId         int,
+    @FirstId                int,
+    @SecondId               int,
+    @ThirdId                int,
+    @ThemeIdHasValue        bit = 1,
+    @PhotographerIdHasValue bit = 1,
+    @FirstIdHasValue        bit = 1,
+    @SecondIdHasValue       bit = 1,
+    @ThirdIdHasValue        bit = 1
+AS
+
+UPDATE [dbo].[PhotographerVoteDetails]
+   SET [ThemeId] =        CASE WHEN @ThemeIdHasValue        = 1 THEN @ThemeId        ELSE [ThemeId]        END
+      ,[FirstId] =        CASE WHEN @FirstIdHasValue        = 1 THEN @FirstId        ELSE [FirstId]        END
+      ,[SecondId] =       CASE WHEN @SecondIdHasValue       = 1 THEN @SecondId       ELSE [SecondId]       END
+      ,[ThirdId] =        CASE WHEN @ThirdIdHasValue        = 1 THEN [ThirdId]       ELSE [SecondId]       END
+      ,[PhotographerId] = CASE WHEN @PhotographerIdHasValue = 1 THEN @PhotographerId ELSE [PhotographerId] END
+	WHERE [Id] = @Id
+
+RETURN 0
