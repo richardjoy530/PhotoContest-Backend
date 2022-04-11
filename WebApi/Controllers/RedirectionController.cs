ad157fa18c7ea3892a085166e5b70b46748eec7d
Richard joy <richardjoy530@gmail.com>
2022-04-11 08:16:26 +0530

    Redirection to swagger ui

---
+++ WebApi/Controllers/RedirectionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class RedirectionController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult RedirectToSwagger()
+        {
+            return RedirectPermanent("swagger");
+        }
+    }
+}
